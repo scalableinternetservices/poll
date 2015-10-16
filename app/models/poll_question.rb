d8dcaf8735af189b0d81e1bbb0db2e26a655e362
Roy Xia <royxia062@gmail.com>
2015-10-16 16:12:48 -0700

    Added cascading deletes

--- app/models/poll_question.rb
+++ app/models/poll_question.rb
@@ -1,6 +1,6 @@
 class PollQuestion < ActiveRecord::Base
   belongs_to :user_poll
-  has_many :answers
+  has_many :answers, dependent: :destroy
   accepts_nested_attributes_for  :answers
 
   @@MAX_NUM_ANSWERS = 4
--- app/models/user_poll.rb
+++ app/models/user_poll.rb
@@ -1,7 +1,7 @@
 class UserPoll < ActiveRecord::Base
-  has_many(:poll_questions, dependent: :destroy)
-  belongs_to(:user)
-  accepts_nested_attributes_for(:poll_questions)
+  has_many :poll_questions, dependent: :destroy
+  belongs_to :user
+  accepts_nested_attributes_for :poll_questions
 
   @@MAX_NUM_POLL_QUESTIONS = 10
 
