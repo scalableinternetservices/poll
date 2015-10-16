dee737d9da789807acdde0b3f2573d82a5ec09af
Roy Xia <royxia062@gmail.com>
2015-10-16 10:21:59 -0700

    Included functionality to add up to 10 poll options

---
+++ app/assets/javascripts/user_polls.js
@@ -0,0 +1,19 @@
+function addPollOption() {
+    $(".hidden_field").each( function(index) {
+        // Just a little hacky
+        if ($(this).css("display") == "block")
+            return true;
+
+        $(this).css("display", "block");
+        return false;
+    });
+
+    // Also a little hacky; hide the button once all options are displayed
+    $("#add_poll_option_button").css("display", "none");
+    $(".hidden_field").each( function(index) {
+        if ($(this).css("display") == "none") {
+            $("#add_poll_option_button").css("display", "block");
+            return false;
+        }
+    });
+}
