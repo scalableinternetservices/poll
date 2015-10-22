f95f5c0a4b0f9e51b9ff73967223ebd642eb05dc
Ashish Lal <ridininmybenz44@gmail.com>
2015-10-21 23:43:44 -0700

    Including db schema for editing user profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016221415) do
+ActiveRecord::Schema.define(version: 20151022053325) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "poll_question_id"
@@ -54,6 +54,9 @@ ActiveRecord::Schema.define(version: 20151016221415) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.text     "description"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
