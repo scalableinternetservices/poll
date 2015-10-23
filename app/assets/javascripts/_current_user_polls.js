537fc6b0d4a1a8b3d526400a73358d47230183a2
Roy Xia <royxia062@gmail.com>
2015-10-22 17:02:40 -0700

    Fixed problem with Turbolinks. Added show more for user polls.

---
+++ app/assets/javascripts/_current_user_polls.js
@@ -0,0 +1,14 @@
+$(document).on("page:change", setupShowMoreCurrentUserPolls);
+
+function setupShowMoreCurrentUserPolls() {
+    $("button[data-current-user-polls-request-size]").click(function () {
+        // Submit an AJAX request for a new poll table
+        $.get("user_polls/current_user_polls",
+             { num_current_user_polls: this.getAttribute("data-current-user-polls-request-size") },
+             function(data) {
+                 $("#current-user-polls").html(data)
+                 setupShowMoreCurrentUserPolls();
+             },
+             "html");
+    });
+}
