5b7b0147a534532030d55e640cf8338ca1211d2f
akhilnadendla <anadendla@g.ucla.edu>
2015-10-23 21:45:44 +0000

    added mysql

--- db/schema.rb
+++ db/schema.rb
@@ -14,52 +14,55 @@
 ActiveRecord::Schema.define(version: 20151022053325) do
 
   create_table "answers", force: :cascade do |t|
-    t.integer  "poll_question_id"
-    t.text     "text"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.integer  "poll_question_id", limit: 4
+    t.text     "text",             limit: 65535
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
-  add_index "answers", ["poll_question_id"], name: "index_answers_on_poll_question_id"
+  add_index "answers", ["poll_question_id"], name: "index_answers_on_poll_question_id", using: :btree
 
   create_table "poll_questions", force: :cascade do |t|
-    t.integer  "user_poll_id"
-    t.text     "text"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "user_poll_id", limit: 4
+    t.text     "text",         limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  add_index "poll_questions", ["user_poll_id"], name: "index_poll_questions_on_user_poll_id"
+  add_index "poll_questions", ["user_poll_id"], name: "index_poll_questions_on_user_poll_id", using: :btree
 
   create_table "user_polls", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "user_id",     limit: 4
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  add_index "user_polls", ["user_id"], name: "index_user_polls_on_user_id"
+  add_index "user_polls", ["user_id"], name: "index_user_polls_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255,   default: "", null: false
+    t.string   "encrypted_password",     limit: 255,   default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.text     "description"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.text     "description",            limit: 65535
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "answers", "poll_questions"
+  add_foreign_key "poll_questions", "user_polls"
+  add_foreign_key "user_polls", "users"
 end
