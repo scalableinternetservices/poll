597655933b0746bd25c847918636457e1eecfada
Roy Xia <royxia062@gmail.com>
2015-10-29 20:52:22 -0700

    Semi-WIP, cleaned up the form a little.

--- app/assets/javascripts/_news_feed_polls.js
+++ app/assets/javascripts/_news_feed_polls.js
@@ -1,7 +1,7 @@
 $(document).on("page:change", setupShowMoreNewsFeedPolls);
 
 function setupShowMoreNewsFeedPolls() {
-    $("button[data-news-feed-polls-request-size]").click(function () {
+    $("button[data-news-feed-polls-request-size]").off().click(function () {
         // Submit an AJAX request for a new poll table
         $.get("user_polls/news_feed_polls",
              { num_news_feed_polls: this.getAttribute("data-news-feed-polls-request-size") },
