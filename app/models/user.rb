8bf7f10ac90e52f11325afcbf81bad003b307d9b
Ashish Lal <ridininmybenz44@gmail.com>
2015-11-13 10:38:31 -0800

    [#107085700] Added in ability to upload profile pictures for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+  }, :default_url => "/images/:style/placeholder.png"
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :profile_picture, :content_type => /\Aimage\/.*\Z/
